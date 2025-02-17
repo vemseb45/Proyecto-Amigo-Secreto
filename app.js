3a707f745e3cc63fb0d1c05f620cca8a0c2ff278
Sebastian <olimacgamer1236@gmail.com>
2025-02-17 18:41:11 -0500

    se agrega parte logica

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+function agregarElementoLista(nombre) {
+    let listaHTML = document.getElementById("listaAmigos"); // Seleccionamos la lista <ul>
+    let nuevoElemento = document.createElement("li"); // Creamos un nuevo <li>
+    nuevoElemento.textContent = nombre; // Asignamos el texto del nombre
+    listaHTML.appendChild(nuevoElemento); // Agregamos el <li> a la lista
+    return;
+}
+
+
+function limpiarInput(){
+    document.querySelector('#amigo').value= ''; 
+    return;
+}
+    
+function modificacionElemento(elemento, texto){
+    let elementohtml = document.querySelector(elemento);
+    elementohtml.innerHTML = texto;
+    return; 
+}
+
+function botonAñadir(){
+    let nombre = document.getElementById("amigo").value;
+    if (nombre.trim() === ''){
+        modificacionElemento('h2', 'Por favor, inserta un nombre.');
+        return;
+    }
+    else {
+        limpiarInput();
+        agregarElementoLista(nombre);
+        modificacionElemento('h2', 'Escribe otro nombre');
+        listaAmigos.push(nombre);
+        return;
+    }   
+    
+}
+  
+
+
+
+
